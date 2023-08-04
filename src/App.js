1de7b94483ecb5491cd72d2fa7d0219f2f8b811d
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-04 19:37:42 +0530

    Money List Design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-
+import "./app.css";
+import { MoneyPyramid } from "./Data";
 
 function App() {
-  return <div className="App">React app</div>;
+  return (
+    <div className="app">
+      <div className="main"></div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {MoneyPyramid.map((e) => (
+            <li className="moneyListItem">
+              <span className="moneyListItemNumber">{e.id}</span>
+              <span className="moneyListItemAmount">{e.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,62 @@
+export const MoneyPyramid = [
+  {
+    id: 1,
+    amount: "₹ 10000",
+  },
+  {
+    id: 2,
+    amount: "₹ 20000",
+  },
+  {
+    id: 3,
+    amount: "₹ 30000",
+  },
+  {
+    id: 4,
+    amount: "₹ 40000",
+  },
+  {
+    id: 5,
+    amount: "₹ 50000",
+  },
+  {
+    id: 6,
+    amount: "₹ 60000",
+  },
+  {
+    id: 7,
+    amount: "₹ 70000",
+  },
+  {
+    id: 8,
+    amount: "₹ 80000",
+  },
+  {
+    id: 9,
+    amount: "₹ 90000",
+  },
+  {
+    id: 10,
+    amount: "₹ 100000",
+  },
+  {
+    id: 11,
+    amount: "₹ 110000",
+  },
+  {
+    id: 12,
+    amount: "₹ 120000",
+  },
+  {
+    id: 13,
+    amount: "₹ 130000",
+  },
+  {
+    id: 14,
+    amount: "₹ 140000",
+  },
+  {
+    id: 15,
+    amount: "₹ 150000",
+  },
+].reverse();
