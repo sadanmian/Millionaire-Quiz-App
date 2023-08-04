f3d81bf1a3d16fbf2a30b6a124e07c0700dbd609
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-04 20:43:08 +0530

    Designing main container

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
   return (
     <div className="app">
-      <div className="main"></div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">question and answers</div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
           {MoneyPyramid.map((e) => (
